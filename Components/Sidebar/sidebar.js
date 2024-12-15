6aaeaa18d26e482956a907943fa783d29fe68505
ashfoste <ashton.foster@du.edu>
2024-12-14 18:50:51 -0800

    fiked a sidebar error and am working on a photo

--- Components/Sidebar/sidebar.js
+++ Components/Sidebar/sidebar.js
@@ -7,7 +7,12 @@ function closeSidebar(){
 }
 
 function toggleSidebar(){
-    if ( document.getElementById("sidebar").style.width === '0em'){
+
+    // for some reason the frst time the webpage opens the width is not undefined but set to '' 
+    // this caused the user to have to pres the open button twice when first clicking before i addedded this fix
+    if(document.getElementById("sidebar").style.width == ''){
+        openSidebar();
+    }else if ( document.getElementById("sidebar").style.width === "0em"){
         openSidebar();
     }else{
         closeSidebar();
